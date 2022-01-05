a63c793d363330ab5e466b3765e348d04eac235b
AsifMohammedSifat <asifmohammedsifat38@gmail.com>
2022-01-05 16:49:48 +0600

    fixed login issue

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,18 +2,18 @@ const router = require('express').Router()
 const userController = require('../controllers/userController')
 const auth = require('../middleware/auth')
 
-router.post('https://intense-lake-37547.herokuapp.com/register', userController.register)
+router.post('https://intense-lake-37547.herokuapp.com/user/register', userController.register)
 
-router.post('https://intense-lake-37547.herokuapp.com/login', userController.login)
+router.post('https://intense-lake-37547.herokuapp.com/user/login', userController.login)
 
-router.get('https://intense-lake-37547.herokuapp.com/logout', userController.logout)
+router.get('https://intense-lake-37547.herokuapp.com/user/logout', userController.logout)
 
-router.get('https://intense-lake-37547.herokuapp.com/refresh_token', userController.refreshToken)
+router.get('https://intense-lake-37547.herokuapp.com/user/refresh_token', userController.refreshToken)
 
-router.get('https://intense-lake-37547.herokuapp.com/infor', auth, userController.getUser)
+router.get('https://intense-lake-37547.herokuapp.com/user/infor', auth, userController.getUser)
 
-router.patch('https://intense-lake-37547.herokuapp.com/addcart', auth, userController.addToCart)
+router.patch('https://intense-lake-37547.herokuapp.com/user/addcart', auth, userController.addToCart)
 
-router.get('https://intense-lake-37547.herokuapp.com/history', auth, userController.history)
+router.get('https://intense-lake-37547.herokuapp.com/user/history', auth, userController.history)
 
 module.exports = router
